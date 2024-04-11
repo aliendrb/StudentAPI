e16a756c0361921bb969cf89eac5e23a47304995
Kacper Januszewski <kacper.januszewski@gmail.com>
2024-04-11 02:03:52 +0200

    set up Visits

---
+++ Classes/Visit.cs
@@ -0,0 +1,23 @@
+﻿using VetAPI.Classes;
+namespace VetAPI.Classes
+{
+    public class Visit
+    {
+        public DateTime Date { get; set; }
+        public Animal Animal { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
+/*
+ * 
+ * 
+ * 
+ * Add controller
+ * Another DB???
+ * get and post methods with specific animal id???
+ * 
+ * 
+ * 
+ * 
+ */
